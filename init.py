a8c22f5485a80b033d675474d8f12b754330c183
kimberks <skimberk1@gmail.com>
2020-01-30 11:23:20 -0600

    updated hello world message

--- init.py
+++ init.py
@@ -41,7 +41,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('hello world 2.0\n\n'.encode())
+        self.wfile.write('hello world 3.0\n\n'.encode())
 
         self.wfile.write(('path: ' + self.path).encode())
 
