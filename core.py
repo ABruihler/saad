b19284a7d866c89b551ee45cf5d64c74862b3795
David White <whited@carleton.edu>
2020-02-06 10:26:24 -0600

    Wrote skeleton for core function

---
+++ core.py
@@ -0,0 +1,16 @@
+import json
+def parseProbeFile():
+    return True
+
+def iterateOverProbes():
+    return True
+
+def runActuatorScript():
+    return True
+
+def findProbeTarget():
+    #Example: Run AST script
+    return True
+
+def runProbeScript():
+    return False
