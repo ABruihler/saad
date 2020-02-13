8e234b8d762e298f096e89ff79a839cbddfac667
David White <whited@carleton.edu>
2020-02-13 11:23:35 -0600

    Disable probes for now

--- init.py
+++ init.py
@@ -114,7 +114,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
                     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
                     # TODO: call
-                    core.iterate_over_probe_files(current_dirname, previous_dirname)
+                    # core.iterate_over_probe_files(current_dirname, previous_dirname)
                     # in core.py
 
             print(clone_url, ref, previous_commit, current_commit)
