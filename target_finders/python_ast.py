9a0d7577fe75dfda2a84963a97981ee10457ca63
Sebastian Kimberk <skimberk1@gmail.com>
2020-02-13 13:55:58 -0500

    Got ast mostly working

--- target_finders/python_ast.py
+++ target_finders/python_ast.py
@@ -1,5 +1,32 @@
 import ast
 import asttokens
+import re
+from collections import namedtuple
+
+token_sub_regex = r'[^\s\(\)]+'
+token_regex = r'^' + token_sub_regex + r'$'
+named_token_regex = r'^(' + token_sub_regex + r')\((' + token_sub_regex + r')\)$'
+
+Token = namedtuple('Token', 'type direct_child')
+NamedToken = namedtuple('NamedToken', 'type name direct_child')
+
+def parse_ast_location(location_str):
+    tokens = location_str.split()
+    parsed = []
+    direct_child = False
+
+    for token in tokens:
+        if token == '>':
+            direct_child = True
+        elif re.match(token_regex, token):
+            parsed.append(Token(token, direct_child))
+            direct_child = False
+        elif re.match(named_token_regex, token):
+            match = re.match(named_token_regex, token)
+            parsed.append(NamedToken(match.group(1), match.group(2), direct_child))
+            direct_child = False
+
+    return parsed
 
 def iter_fields(node):
     """
@@ -27,22 +54,49 @@ def node_to_string(node):
 
     return out
 
-class Visitor(ast.NodeVisitor):
-    def generic_visit(self, node, deep=0):
-        """Called if no explicit visitor function exists for a node."""
-        prefix = '-' * deep
-        print(prefix + node_to_string(node))
+class Visitor():
+    # def old_visit(self, node, deep=0):
+    #     """Called if no explicit visitor function exists for a node."""
+    #     prefix = '-' * deep
+    #     print(prefix + node_to_string(node))
+
+    #     for field, value in iter_fields(node):
+    #         if isinstance(value, list):
+    #             for item in value:
+    #                 if isinstance(item, ast.AST):
+    #                     self.generic_visit(item, deep + 1)
+    #         elif isinstance(value, ast.AST):
+    #             self.generic_visit(value, deep + 1)
+
+    def visit(self, node, location):
+        target = location[0]
+
+        if target.type == 'class':
+            if isinstance(node, ast.ClassDef) and node.name == target.name:
+                location = location[1:]
+            elif target.direct_child:
+                return
+        elif target.type == 'func':
+            if isinstance(node, ast.FunctionDef) and node.name == target.name:
+                location = location[1:]
+            elif target.direct_child:
+                return
+
+        if len(location) == 0:
+            print(node)
+            return
 
         for field, value in iter_fields(node):
             if isinstance(value, list):
                 for item in value:
                     if isinstance(item, ast.AST):
-                        self.generic_visit(item, deep + 1)
+                        self.visit(item, location)
             elif isinstance(value, ast.AST):
-                self.generic_visit(value, deep + 1)
+                self.visit(value, location)
 
 with open(__file__, 'rb') as src_stream:
     source = src_stream.read()
     atok = asttokens.ASTTokens(source, parse=True)
     visitor = Visitor()
-    visitor.visit(atok.tree)
+    location = parse_ast_location('class(Visitor) > func(visit)')
+    visitor.visit(atok.tree, location)
