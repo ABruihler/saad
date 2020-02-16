44b6128f3c5f47a62a78ffcf0e328a78291bb949
Ari Conati <conatia@carleton.edu>
2020-02-16 15:21:48 -0600

    Added command line functionality for mail actuator

--- actuators/mail_actuator.py
+++ actuators/mail_actuator.py
@@ -1,14 +1,13 @@
 import smtplib
+import sys
 import getpass
 
 from email.message import EmailMessage
 
-# should initialize the following on startup:
+# should initialize the following on startup / in config:
 
 SMTP_SERVER = 'smtp.gmail.com'
-SENDER_MAIL = ''
 PORT = 465
-PASSWORD = ''
 
 def send_email(sender_mail, receiver_mail, message, smtp_server=SMTP_SERVER):
     password = getpass.getpass('Password: ') # temporary / for testing
@@ -25,3 +24,21 @@ def send_email(sender_mail, receiver_mail, message, smtp_server=SMTP_SERVER):
     server.login(sender_mail, password)
     server.sendmail(sender_mail, receiver_mail, msg.as_string())
     server.quit()
+
+
+def main():
+    sender_mail = sys.argv[1] 
+    receiver_mail = sys.argv[2]
+    message = sys.argv[3]
+
+    send_email(sender_mail, receiver_mail, message)
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        pass
+    if sys.argv.__len__() != 4:
+        print("Usage: mail_actuator.py [SMTP_SERVER] [SENDER_EMAIL_ADDRESS] [RECEIVER_EMAIL_ADDRESS] [MESSAGE]")
+    else:
+        main()
+
