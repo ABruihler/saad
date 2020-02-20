b14066d23b884999ded29523b18ce55d001c19b5
Sebastian Kimberk <skimberk1@gmail.com>
2020-02-20 11:01:28 -0500

    Simplified code by factoring out json file iteration

--- module_runner.py
+++ module_runner.py
@@ -5,25 +5,37 @@ import shlex
 import os
 import re
 
+# Replace curly-brace surrounded variables with
+# the corresponding value in 'values'
+# Example: insert_named_valued('Hello {name}', {'name': 'Bob'})
+# -> 'Hello Bob'
+def insert_named_values(string, values):
+    return re.sub(r'{([a-zA-Z0-9_]+)}', lambda m: values[m.group(1)], string)
+
 def parse_json_file(file_path):
     with open(file_path, 'r') as probe_config_file:
         probe_file_contents = probe_config_file.read()
 
     return json.loads(probe_file_contents)
 
-def load_modules():
-    modules_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modules')
-
+# Easy way to recursively iterate over all json files in a directory
+# Usage: for parsed_json in all_json_in_dir(path):
+def all_json_in_dir(dir_path):
     # Search recursively in order to allow user to decide
     # their preferred method of organization
-    pathlist = Path(modules_dir).glob('**/*.json')
-
-    modules = {}
-
+    pathlist = Path(dir_path).glob('**/*.json')
     for path in pathlist:
         parsed = parse_json_file(str(path))
-        for key, value in parsed.items():
+        yield parsed
+
+def load_modules():
+    modules = {}
+
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modules')
+    for config in all_json_in_dir(path):
+        for key, value in config.items():
             if key in modules:
+                # Module of same type was already defined somewhere else
                 raise ValueError('Duplicate module')
 
             modules[key] = value
@@ -31,9 +43,6 @@ def load_modules():
     return modules
 
 modules = load_modules()
-
-def insert_named_values(string, values):
-    return re.sub(r'{([a-zA-Z0-9_]+)}', lambda m: values[m.group(1)], string)
 
 def run_module(module_type, config, bound_values):
     module = modules[module_type]
@@ -56,9 +65,7 @@ def run_module(module_type, config, bound_values):
         print(output)
         return output
 
-def handle_config(file_path, current_commit_dir, previous_commit_dir):
-    print('Handling ', file_path)
-    config = parse_json_file(file_path)
+def handle_config(config, current_commit_dir, previous_commit_dir):
     bound_values = {}
 
     for module in config:
@@ -67,14 +74,8 @@ def handle_config(file_path, current_commit_dir, previous_commit_dir):
             bound_values[module['name']] = output
 
 def iterate_over_configs(current_commit_dir, previous_commit_dir):
-    probes_dir = os.path.join(current_commit_dir, 'monitoring')
-
-    # Search recursively in order to allow user to decide
-    # their preferred method of organization
-    pathlist = Path(probes_dir).glob('**/*.json')
-
-    for path in pathlist:
-        path_str = str(path)
-        handle_config(path_str, current_commit_dir, previous_commit_dir)
+    path = os.path.join(current_commit_dir, 'monitoring')
+    for config in all_json_in_dir(path):
+        handle_config(config, current_commit_dir, previous_commit_dir)
 
 iterate_over_configs(os.path.dirname(os.path.abspath(__file__)), os.path.dirname(os.path.abspath(__file__)))
